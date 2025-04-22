962f5e87552422b3f120e7a414a6b35ce93178af
tmchrt <tom.chareton@gmail.com>
2025-04-22 11:52:42 +0200

    exception

--- Bureaucrat.hpp
+++ Bureaucrat.hpp
@@ -10,7 +10,23 @@ private:
     int grade;
 public:
     Bureaucrat(const std::string name, int grade);
+    Bureaucrat(const Bureaucrat &other);
     ~Bureaucrat();
+    Bureaucrat &operator=(const Bureaucrat &other);
+    std::string getName() const;
+    int getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
+
+    class GradeTooHighException : public std::exception{
+        public:
+        const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception{
+        public:
+        const char *what() const throw();
+    };
 };
 
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
 #endif
